97662ecc0c11d59ed151d8017ab6bbf906ac1370
Law-son <buabassahlawson@gmail.com>
2025-11-27 01:01:00 +0000

    feat(api): add transactable interface for payment processing

--- src/main/java/bankapp/models/transactions/Transactable.java
+++ src/main/java/bankapp/models/transactions/Transactable.java
@@ -1,4 +1,6 @@
 package main.java.bankapp.models.transactions;
 
 public interface Transactable {
+    // Returns boolean to indicate success/failure
+    boolean processTransaction(double amount, String type);
 }
